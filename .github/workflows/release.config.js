a3eeb8a1b71e04e117e47ca823afcef3636217a3
Kannan <394277@cognizant.com>
2021-07-05 11:24:34 +0530

    fix: fix description

---
+++ .github/workflows/release.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  branches: "master",
+  repositoryUrl: "https://github.com/Ragu-Migritech/react-app",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github"
+  ]
+}
